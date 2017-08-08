425d52df5ed7f66efb9381eef4cfe2a8ddb3eb22
Till Emme <till.emme@stud.uni-regensburg.de>
2017-08-08 11:46:59 +0200

    Adding calculation of classes

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -1,8 +1,18 @@
-bsp = [([4, 5, 4, 5], 4, 5), ([3, 4, 5, 4], 4)]
+bsp = [([4, 5, 4, 5], 4), ([3, 4, 5, 4], 5)]
 
 
 class NaiveBayesClassifier:
     def train(self, labeled_trainingset):
         "Trainingset:[([4,5,4,5],Klasse),...]"
         ProbClasses = {}
-        for set in labeled_trainingset:
+        for s in labeled_trainingset:
+            if s[1] not in ProbClasses:
+                ProbClasses[s[1]] = 1
+            else:
+                ProbClasses[s[1]]+=1
+        for c in ProbClasses:
+            ProbClasses[c] /= len(labeled_trainingset)
+        print(ProbClasses)
+
+classifier = NaiveBayesClassifier()
+classifier.train(bsp)

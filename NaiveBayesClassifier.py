cc124b27fc609a4076f26905e31875447ce6109d
Till Emme <till.emme@stud.uni-regensburg.de>
2017-08-08 12:44:42 +0200

    Adding Feature Probability Calculation

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -1,18 +1,45 @@
-bsp = [([4, 5, 4, 5], 4), ([3, 4, 5, 4], 5)]
+bsp = [([4, 5, 4, 5], 4), ([3, 4, 5, 4], 5), ([2, 3, 4, 5], 4)]
 
 
 class NaiveBayesClassifier:
+    def __init__(self):
+        self.ProbClasses = {}
+        self.ProbFeatures = {}
+
     def train(self, labeled_trainingset):
         "Trainingset:[([4,5,4,5],Klasse),...]"
-        ProbClasses = {}
+        self.calculateClassProbabilities(labeled_trainingset)
+        self.calculateFeatureProbabilities(labeled_trainingset)
+
+    def calculateClassProbabilities(self, labeled_trainingset):
         for s in labeled_trainingset:
-            if s[1] not in ProbClasses:
-                ProbClasses[s[1]] = 1
+            if s[1] not in self.ProbClasses:
+                self.ProbClasses[s[1]] = 1
             else:
-                ProbClasses[s[1]]+=1
-        for c in ProbClasses:
-            ProbClasses[c] /= len(labeled_trainingset)
-        print(ProbClasses)
+                self.ProbClasses[s[1]] += 1
+        for c in self.ProbClasses:
+            self.ProbClasses[c] /= len(labeled_trainingset)
+
+    def calculateFeatureProbabilities(self, labeled_trainingset):
+        for c in self.ProbClasses:
+            features = {}
+            for set in labeled_trainingset:
+                if set[1] == c:
+                    for data in set[0]:
+                        if data not in features:
+                            features[data] = 1
+                        else:
+                            features[data] += 1
+            self.ProbFeatures[c] = features
+        print(self.ProbFeatures)
+        for c in self.ProbFeatures:
+            features = {}
+            for feature in self.ProbFeatures[c]:
+                features[feature] = self.ProbFeatures[c][feature] / sum(self.ProbFeatures[c].values())
+                print(c)
+            self.ProbFeatures[c] = features
+        print(self.ProbFeatures)
+
 
 classifier = NaiveBayesClassifier()
 classifier.train(bsp)
